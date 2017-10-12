bc5c7e33b4dd08d3212c4630ff80280792792030
doublemix <32748075+doublemix@users.noreply.github.com>
2017-10-12 15:17:57 -0400

    Added yet another line

--- hello-git/src/edu/lhup/comp405/HelloGit.java
+++ hello-git/src/edu/lhup/comp405/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello, and welcome to the world of Git.");
 		System.out.println("Good-bye");
+		System.out.println("Yet another line");
 	}
 
 }
